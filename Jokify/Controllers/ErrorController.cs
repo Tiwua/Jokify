470f61b87778c578172e39e44b77a7fb434f5186
Tiwua <Tiwua@students.softuni.bg>
2023-08-11 03:05:54 +0300

    header bug fix around views And Error Controller implemented with error handler

---
+++ Jokify/Controllers/ErrorController.cs
@@ -0,0 +1,21 @@
+﻿namespace Jokify.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    public class ErrorController : BaseController
+    {
+        public IActionResult Error(int statusCode)
+        {
+            if (statusCode == 404)
+            {
+                return View("Error404");
+            }
+            else if (statusCode == 401)
+            {
+                return View("Error401");
+            }
+
+            return View("Error");
+        }
+    }
+}
--- Jokify/Program.cs
+++ Jokify/Program.cs
@@ -2,79 +2,93 @@ namespace Jokify
 {
     using Jokify.Extensions;
     using Jokify.Infrastructure.Data;
-	using Jokify.Infrastructure.Data.Models;
-	using static Jokify.Areas.Admin.Constants.AdminConstants;
+    using Jokify.Infrastructure.Data.Models;
+    using static Jokify.Areas.Admin.Constants.AdminConstants;
     using Microsoft.AspNetCore.Identity;
     using Microsoft.EntityFrameworkCore;
     using System.Security.Claims;
-	using Jokify.ActionFilters.FIlters;
+    using Jokify.ActionFilters.FIlters;
 
-	public class Program
+    public class Program
     {
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
-			var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
-			builder.Services.AddDbContext<JokifyDbContext>(options =>
-				options.UseSqlServer(connectionString));
-			builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+            var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+            builder.Services.AddDbContext<JokifyDbContext>(options =>
+                options.UseSqlServer(connectionString));
+            builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-			builder.Services.AddDefaultIdentity<User>(options =>
-			{
-				options.SignIn.RequireConfirmedAccount = false;
-				options.Password.RequireDigit = false;
-				options.Password.RequireLowercase = false;
-				options.Password.RequireUppercase = false;
-				options.Password.RequireNonAlphanumeric = false;
-				options.Password.RequiredLength = 6;
-			})
-				.AddRoles<IdentityRole>()
-				.AddEntityFrameworkStores<JokifyDbContext>();
+            builder.Services.AddDefaultIdentity<User>(options =>
+            {
+                options.SignIn.RequireConfirmedAccount = false;
+                options.Password.RequireDigit = false;
+                options.Password.RequireLowercase = false;
+                options.Password.RequireUppercase = false;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequiredLength = 6;
+            })
+                .AddRoles<IdentityRole>()
+                .AddEntityFrameworkStores<JokifyDbContext>();
 
-			builder.Services.ConfigureApplicationCookie(options =>
-			{
-				options.LoginPath = "/User/Login";
-			});
+            builder.Services.ConfigureApplicationCookie(options =>
+            {
+                options.LoginPath = "/User/Login";
+                options.AccessDeniedPath = "/Home/Error/404";
+            });
 
-			builder.Services.AddControllersWithViews(options =>
-			{
-				options.Filters.Add(typeof(DynamicLayoutActionFilter));
-			});
+            builder.Services.AddControllersWithViews(options =>
+            {
+                options.Filters.Add(typeof(DynamicLayoutActionFilter));
+            });
 
-			builder.Services.AddApplicationServices();
+            builder.Services.AddApplicationServices();
             builder.Services.Configure<IdentityOptions>(options => options.ClaimsIdentity.UserIdClaimType = ClaimTypes.NameIdentifier);
-			builder.Services.AddHttpContextAccessor();
+            builder.Services.AddHttpContextAccessor();
 
             var app = builder.Build();
 
-			if (app.Environment.IsDevelopment())
-			{
-				app.UseMigrationsEndPoint();
-			}
-			else
-			{
-				app.UseExceptionHandler("/Home/Error");
-				app.UseHsts();
-			}
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Error/Error");
+            }
+            else
+            {
+                app.UseHsts();
+            }
 
-			app.UseHttpsRedirection();
-			app.UseStaticFiles();
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
 
-			app.UseRouting();
+            app.UseRouting();
 
-			app.UseAuthentication();
-			app.UseAuthorization();
+            app.UseAuthentication();
+            app.UseAuthorization();
 
-			app.SeedAdmin(AdminEmail);
+            app.SeedAdmin(AdminEmail);
 
-			app.MapControllerRoute(
-				name: "default",
-				pattern: "{controller=Home}/{action=Index}/{id?}");
-			app.MapRazorPages();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
 
-			app.Run();
-		}
-	}
+                endpoints.MapControllerRoute(
+                    name: "error",
+                    pattern: "Error/{statusCode}",
+                    defaults: new { controller = "Error", action = "Error" });
+
+                endpoints.MapControllerRoute(
+                    name: "error",
+                    pattern: "{*url}",
+                    defaults: new { controller = "Error", action = "Error" });
+            });
+
+            app.MapRazorPages();
+
+            app.Run();
+        }
+    }
 }
